f6eea88ae1b66e69ff7aa8d5ec59260af8aae945
Chris Grivas <chgrivas7@gmail.com>
2018-07-25 00:15:44 +0300

    Add first implementation with tests

--- sample/src/main/scala/com/chgrivas/scalarat/kata/karatechop/Karate.scala
+++ sample/src/main/scala/com/chgrivas/scalarat/kata/karatechop/Karate.scala
@@ -2,7 +2,25 @@ package com.chgrivas.scalarat.kata.karatechop
 
 object Karate {
 
-  def chop(target: Int, sortedArray: Array[Int]): Int = {
-    2
+  /**
+    * First attempt. Let's do it with a loop.
+     */
+  def chop(t: Int, arr: Array[Int]): Int = {
+    var min = 0
+    var max = arr.length -1
+
+    var guess = -1
+
+    while (min <= max) {
+      var guess = (max - min) / 2
+
+      if (t == arr(guess))
+        return guess
+      else if (t > arr(guess))
+        min = guess + 1
+      else
+        max = guess -1
+    }
+    -1
   }
 }
--- sample/src/test/scala/com/chgrivas/scalarat/kata/karatechop/KarateSpec.scala
+++ sample/src/test/scala/com/chgrivas/scalarat/kata/karatechop/KarateSpec.scala
@@ -7,4 +7,16 @@ class KarateSpec extends FunSuite with DiagrammedAssertions {
   test("Calling the chop method of the companion objectcompiles successfully") {
     assertCompiles("Karate.chop(1, Array.emptyIntArray)")
   }
+
+  test("Searching in an emmpty array returns -1") {
+    assert(Karate.chop(1, Array.emptyIntArray) == -1)
+  }
+
+  test("Searching in a single element array a non existing value returns -1") {
+    assert(Karate.chop(1, Array(10)) == -1)
+  }
+
+  test("Searching in a single element array an existing value returns the correct index") {
+    assert(Karate.chop(10, Array(10)) == 0)
+  }
 }
