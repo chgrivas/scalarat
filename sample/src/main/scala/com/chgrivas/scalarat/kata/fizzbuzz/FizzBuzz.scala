8c08ed71e315d1e9462be2b006452a8ca686ca9c
Chris Grivas <chgrivas7@gmail.com>
2018-07-25 23:30:43 +0300

    Fix literals

--- sample/src/main/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzz.scala
+++ sample/src/main/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzz.scala
@@ -6,11 +6,11 @@ object FizzBuzz {
     require(n > 0)
 
     if (n % 3 == 0 && n % 5 == 0)
-      return "FizzBuzz"
+      return "fizzbuzz"
     else if (n % 3 == 0)
-      return "Fizz"
+      return "fizz"
     else if (n % 5 == 0)
-      return "Buzz"
+      return "buzz"
     n.toString
   }
 }
--- sample/src/test/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzzSpec.scala
+++ sample/src/test/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzzSpec.scala
@@ -13,22 +13,22 @@ class FizzBuzzSpec extends FunSuite with DiagrammedAssertions {
     assertThrows[IllegalArgumentException](FizzBuzz.print(0))
   }
 
-  test("FizzBuzz with a number that gets divided by 3 should return Fizz") {
-    assert(FizzBuzz.print(3) == "Fizz")
-    assert(FizzBuzz.print(6) == "Fizz")
-    assert(FizzBuzz.print(9) == "Fizz")
+  test("FizzBuzz with a number that gets divided by 3 should return fizz") {
+    assert(FizzBuzz.print(3) == "fizz")
+    assert(FizzBuzz.print(6) == "fizz")
+    assert(FizzBuzz.print(9) == "fizz")
   }
 
-  test("FizzBuzz with a number that gets divided by 5 should return Buzz") {
-    assert(FizzBuzz.print(5) == "Buzz")
-    assert(FizzBuzz.print(10) == "Buzz")
-    assert(FizzBuzz.print(20) == "Buzz")
+  test("FizzBuzz with a number that gets divided by 5 should return buzz") {
+    assert(FizzBuzz.print(5) == "buzz")
+    assert(FizzBuzz.print(10) == "buzz")
+    assert(FizzBuzz.print(20) == "buzz")
   }
 
-  test("FizzBuzz with a number that gets divided by both 3 and 5 should return FizzBuzz") {
-    assert(FizzBuzz.print(15) == "FizzBuzz")
-    assert(FizzBuzz.print(30) == "FizzBuzz")
-    assert(FizzBuzz.print(45) == "FizzBuzz")
+  test("FizzBuzz with a number that gets divided by both 3 and 5 should return fizzbuzz") {
+    assert(FizzBuzz.print(15) == "fizzbuzz")
+    assert(FizzBuzz.print(30) == "fizzbuzz")
+    assert(FizzBuzz.print(45) == "fizzbuzz")
   }
 
   test("FizzBuzz with a number not divided by both 3 or 5 should return itself as a string") {
