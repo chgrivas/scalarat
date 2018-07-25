716420122a8a79790c2671df6c13a957d871ffe3
Chris Grivas <chgrivas7@gmail.com>
2018-07-25 23:22:36 +0300

    Add first implementation

--- sample/src/main/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzz.scala
+++ sample/src/main/scala/com/chgrivas/scalarat/kata/fizzbuzz/FizzBuzz.scala
@@ -4,6 +4,13 @@ object FizzBuzz {
 
   def print(n: Int): String = {
     require(n >= 0)
-    ""
+
+    if (n % 3 == 0 && n % 5 == 0)
+      return "FizzBuzz"
+    else if (n % 3 == 0)
+      return "Fizz"
+    else if (n % 5 == 0)
+      return "Buzz"
+    n.toString
   }
 }
