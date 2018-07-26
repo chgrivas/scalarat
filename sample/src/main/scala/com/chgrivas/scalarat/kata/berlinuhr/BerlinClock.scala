f0ec955061f1009da017bed9efce10161396b541
Chris Grivas <chgrivas7@gmail.com>
2018-07-26 20:08:05 +0300

    Add get top hours and infra method

--- sample/src/main/scala/com/chgrivas/scalarat/kata/berlinuhr/BerlinClock.scala
+++ sample/src/main/scala/com/chgrivas/scalarat/kata/berlinuhr/BerlinClock.scala
@@ -7,4 +7,12 @@ object BerlinClock {
   }
 
   def seconds(s: Int): String = if (s % 2 == 0) "Y" else "O"
+
+  def topHours(number: Int) = onOff(4, topNumberOfOnSigns(number))
+
+  private def onOff(lamps: Int, onSigns: Int, onSign: String = "R") = {
+    onSign * onSigns + "O" * (lamps - onSigns)
+  }
+
+  private def topNumberOfOnSigns(number: Int) = (number - (number % 5)) / 5
 }
--- sample/src/test/scala/com/chgrivas/scalarat/kata/berlinuhr/BerlinClockSpec.scala
+++ sample/src/test/scala/com/chgrivas/scalarat/kata/berlinuhr/BerlinClockSpec.scala
@@ -7,6 +7,7 @@ class BerlinClockSpec extends FunSuite with DiagrammedAssertions {
   test("BerlinClock exists") {
     assertCompiles("BerlinClock.convertToBerlinTime(\"15:13:22\")")
     assertCompiles("BerlinClock.seconds(1)")
+    assertCompiles("BerlinClock.topHours(7)")
   }
 
   test("Seconds are Y (yellow only on even seconds)") {
@@ -14,4 +15,17 @@ class BerlinClockSpec extends FunSuite with DiagrammedAssertions {
     assert(BerlinClock.seconds(1) == "O")
     assert(BerlinClock.seconds(49) == "O")
   }
+
+  test("Top hours should have 4 lamps") {
+    assert(BerlinClock.topHours(7).length == 4)
+  }
+
+  test("Top hours is converted to lambs correctly") {
+    assert(BerlinClock.topHours(1) == "OOOO")
+    assert(BerlinClock.topHours(7) == "ROOO")
+    assert(BerlinClock.topHours(14) == "RROO")
+    assert(BerlinClock.topHours(19) == "RRRO")
+    assert(BerlinClock.topHours(22) == "RRRR")
+  }
+
 }
